70f67c05bb87244079aad59e0fce58f0c0bc1ecd
Shadow Board Dev <dev@shadow-board.local>
2026-05-09 14:09:08 +0530

    updated CORS for vercel URL

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://shadow-board.vercel.app"
+    "https://shadow-board-navy.vercel.app"
   ]
 }));
 
